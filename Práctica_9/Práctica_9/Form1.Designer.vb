e2a46e709181ea3fee584341435f2f426a586011
Zashuiba <jorgito.donis@hotmail.com>
2017-04-26 22:55:41 +0200

    Master created

---
+++ Práctica_9/Práctica_9/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.Image = Global.Práctica_9.My.Resources.Resources.k
+        Me.PictureBox9.Location = New System.Drawing.Point(252, 148)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(124, 143)
+        Me.PictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox9.TabIndex = 8
+        Me.PictureBox9.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Image = Global.Práctica_9.My.Resources.Resources.e
+        Me.PictureBox8.Location = New System.Drawing.Point(382, 148)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(124, 143)
+        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox8.TabIndex = 7
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Image = Global.Práctica_9.My.Resources.Resources.Evans_Pi_1
+        Me.PictureBox7.Location = New System.Drawing.Point(122, 297)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(124, 143)
+        Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox7.TabIndex = 6
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = Global.Práctica_9.My.Resources.Resources.lambda
+        Me.PictureBox6.Location = New System.Drawing.Point(252, 297)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(124, 143)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 5
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.Práctica_9.My.Resources.Resources.phi
+        Me.PictureBox5.Location = New System.Drawing.Point(382, 297)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(124, 143)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.Práctica_9.My.Resources.Resources.sqrt_1
+        Me.PictureBox4.Location = New System.Drawing.Point(122, 446)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(124, 143)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.Práctica_9.My.Resources.Resources.sqrt2
+        Me.PictureBox3.Location = New System.Drawing.Point(252, 446)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(124, 143)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.Práctica_9.My.Resources.Resources.y
+        Me.PictureBox2.Location = New System.Drawing.Point(382, 446)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(124, 143)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Práctica_9.My.Resources.Resources.alpha
+        Me.PictureBox1.Location = New System.Drawing.Point(122, 148)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(124, 143)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(669, 630)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+End Class
---
+++ Práctica_9/Práctica_9/Form1.vb
@@ -0,0 +1,559 @@
+﻿Public Class Form1
+
+
+    Dim vis(8) As Boolean
+    Dim clicked(8) As Boolean
+    Dim contador As Integer
+
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+        vis(0) = PictureBox1.Visible
+        vis(1) = PictureBox2.Visible
+        vis(2) = PictureBox3.Visible
+        vis(3) = PictureBox4.Visible
+        vis(4) = PictureBox5.Visible
+        vis(5) = PictureBox6.Visible
+        vis(6) = PictureBox7.Visible
+        vis(7) = PictureBox8.Visible
+        vis(8) = PictureBox9.Visible
+
+        Randomize()
+        Dim value1 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value1) = 0
+        Randomize()
+        Dim value2 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value2) = 0
+        Randomize()
+        Dim value3 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value3) = 0
+        Randomize()
+        Dim value4 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value4) = 1
+
+        Randomize()
+        Dim value5 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value5) = 1
+
+        Randomize()
+        Dim value6 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value6) = 1
+
+        PictureBox1.Visible = vis(0)
+        PictureBox2.Visible = vis(1)
+        PictureBox3.Visible = vis(2)
+        PictureBox4.Visible = vis(3)
+        PictureBox5.Visible = vis(4)
+        PictureBox6.Visible = vis(5)
+        PictureBox7.Visible = vis(6)
+        PictureBox8.Visible = vis(7)
+        PictureBox9.Visible = vis(8)
+
+
+        If (clicked(0) = False) Then
+            contador = contador + 1
+            clicked(0) = True
+        End If
+
+        If (contador = 8) Then
+            MsgBox("Has ganado")
+        End If
+
+    End Sub
+
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+        vis(0) = PictureBox1.Visible
+        vis(1) = PictureBox2.Visible
+        vis(2) = PictureBox3.Visible
+        vis(3) = PictureBox4.Visible
+        vis(4) = PictureBox5.Visible
+        vis(5) = PictureBox6.Visible
+        vis(6) = PictureBox7.Visible
+        vis(7) = PictureBox8.Visible
+        vis(8) = PictureBox9.Visible
+
+        Randomize()
+        Dim value1 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value1) = 0
+        Randomize()
+        Dim value2 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value2) = 0
+        Randomize()
+        Dim value3 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value3) = 0
+        Randomize()
+        Dim value4 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value4) = 1
+
+        Randomize()
+        Dim value5 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value5) = 1
+
+        Randomize()
+        Dim value6 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value6) = 1
+
+        PictureBox1.Visible = vis(0)
+        PictureBox2.Visible = vis(1)
+        PictureBox3.Visible = vis(2)
+        PictureBox4.Visible = vis(3)
+        PictureBox5.Visible = vis(4)
+        PictureBox6.Visible = vis(5)
+        PictureBox7.Visible = vis(6)
+        PictureBox8.Visible = vis(7)
+        PictureBox9.Visible = vis(8)
+
+
+        If (clicked(1) = False) Then
+            contador = contador + 1
+            clicked(1) = True
+        End If
+
+        If (contador = 8) Then
+            MsgBox("Has ganado")
+        End If
+
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+
+        vis(0) = PictureBox1.Visible
+        vis(1) = PictureBox2.Visible
+        vis(2) = PictureBox3.Visible
+        vis(3) = PictureBox4.Visible
+        vis(4) = PictureBox5.Visible
+        vis(5) = PictureBox6.Visible
+        vis(6) = PictureBox7.Visible
+        vis(7) = PictureBox8.Visible
+        vis(8) = PictureBox9.Visible
+
+        Randomize()
+        Dim value1 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value1) = 0
+        Randomize()
+        Dim value2 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value2) = 0
+        Randomize()
+        Dim value3 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value3) = 0
+        Randomize()
+        Dim value4 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value4) = 1
+
+        Randomize()
+        Dim value5 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value5) = 1
+
+        Randomize()
+        Dim value6 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value6) = 1
+
+        PictureBox1.Visible = vis(0)
+        PictureBox2.Visible = vis(1)
+        PictureBox3.Visible = vis(2)
+        PictureBox4.Visible = vis(3)
+        PictureBox5.Visible = vis(4)
+        PictureBox6.Visible = vis(5)
+        PictureBox7.Visible = vis(6)
+        PictureBox8.Visible = vis(7)
+        PictureBox9.Visible = vis(8)
+
+
+        If (clicked(2) = False) Then
+            contador = contador + 1
+            clicked(2) = True
+        End If
+
+        If (contador = 8) Then
+            MsgBox("Has ganado")
+        End If
+
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+
+        vis(0) = PictureBox1.Visible
+        vis(1) = PictureBox2.Visible
+        vis(2) = PictureBox3.Visible
+        vis(3) = PictureBox4.Visible
+        vis(4) = PictureBox5.Visible
+        vis(5) = PictureBox6.Visible
+        vis(6) = PictureBox7.Visible
+        vis(7) = PictureBox8.Visible
+        vis(8) = PictureBox9.Visible
+
+        Randomize()
+        Dim value1 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value1) = 0
+        Randomize()
+        Dim value2 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value2) = 0
+        Randomize()
+        Dim value3 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value3) = 0
+        Randomize()
+        Dim value4 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value4) = 1
+
+        Randomize()
+        Dim value5 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value5) = 1
+
+        Randomize()
+        Dim value6 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value6) = 1
+
+        PictureBox1.Visible = vis(0)
+        PictureBox2.Visible = vis(1)
+        PictureBox3.Visible = vis(2)
+        PictureBox4.Visible = vis(3)
+        PictureBox5.Visible = vis(4)
+        PictureBox6.Visible = vis(5)
+        PictureBox7.Visible = vis(6)
+        PictureBox8.Visible = vis(7)
+        PictureBox9.Visible = vis(8)
+
+
+        If (clicked(3) = False) Then
+            contador = contador + 1
+            clicked(3) = True
+        End If
+
+        If (contador = 8) Then
+            MsgBox("Has ganado")
+        End If
+
+    End Sub
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
+
+        vis(0) = PictureBox1.Visible
+        vis(1) = PictureBox2.Visible
+        vis(2) = PictureBox3.Visible
+        vis(3) = PictureBox4.Visible
+        vis(4) = PictureBox5.Visible
+        vis(5) = PictureBox6.Visible
+        vis(6) = PictureBox7.Visible
+        vis(7) = PictureBox8.Visible
+        vis(8) = PictureBox9.Visible
+
+        Randomize()
+        Dim value1 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value1) = 0
+        Randomize()
+        Dim value2 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value2) = 0
+        Randomize()
+        Dim value3 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value3) = 0
+        Randomize()
+        Dim value4 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value4) = 1
+
+        Randomize()
+        Dim value5 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value5) = 1
+
+        Randomize()
+        Dim value6 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value6) = 1
+
+        PictureBox1.Visible = vis(0)
+        PictureBox2.Visible = vis(1)
+        PictureBox3.Visible = vis(2)
+        PictureBox4.Visible = vis(3)
+        PictureBox5.Visible = vis(4)
+        PictureBox6.Visible = vis(5)
+        PictureBox7.Visible = vis(6)
+        PictureBox8.Visible = vis(7)
+        PictureBox9.Visible = vis(8)
+
+
+        If (clicked(4) = False) Then
+            contador = contador + 1
+            clicked(4) = True
+        End If
+
+        If (contador = 8) Then
+            MsgBox("Has ganado")
+        End If
+
+    End Sub
+
+    Private Sub PictureBox6_Click(sender As Object, e As EventArgs) Handles PictureBox6.Click
+
+        vis(0) = PictureBox1.Visible
+        vis(1) = PictureBox2.Visible
+        vis(2) = PictureBox3.Visible
+        vis(3) = PictureBox4.Visible
+        vis(4) = PictureBox5.Visible
+        vis(5) = PictureBox6.Visible
+        vis(6) = PictureBox7.Visible
+        vis(7) = PictureBox8.Visible
+        vis(8) = PictureBox9.Visible
+
+        Randomize()
+        Dim value1 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value1) = 0
+        Randomize()
+        Dim value2 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value2) = 0
+        Randomize()
+        Dim value3 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value3) = 0
+        Randomize()
+        Dim value4 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value4) = 1
+
+        Randomize()
+        Dim value5 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value5) = 1
+
+        Randomize()
+        Dim value6 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value6) = 1
+
+        PictureBox1.Visible = vis(0)
+        PictureBox2.Visible = vis(1)
+        PictureBox3.Visible = vis(2)
+        PictureBox4.Visible = vis(3)
+        PictureBox5.Visible = vis(4)
+        PictureBox6.Visible = vis(5)
+        PictureBox7.Visible = vis(6)
+        PictureBox8.Visible = vis(7)
+        PictureBox9.Visible = vis(8)
+
+
+        If (clicked(5) = False) Then
+            contador = contador + 1
+            clicked(5) = True
+        End If
+
+        If (contador = 8) Then
+            MsgBox("Has ganado")
+        End If
+
+    End Sub
+
+    Private Sub PictureBox7_Click(sender As Object, e As EventArgs) Handles PictureBox7.Click
+
+        vis(0) = PictureBox1.Visible
+        vis(1) = PictureBox2.Visible
+        vis(2) = PictureBox3.Visible
+        vis(3) = PictureBox4.Visible
+        vis(4) = PictureBox5.Visible
+        vis(5) = PictureBox6.Visible
+        vis(6) = PictureBox7.Visible
+        vis(7) = PictureBox8.Visible
+        vis(8) = PictureBox9.Visible
+
+        Randomize()
+        Dim value1 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value1) = 0
+        Randomize()
+        Dim value2 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value2) = 0
+        Randomize()
+        Dim value3 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value3) = 0
+        Randomize()
+        Dim value4 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value4) = 1
+
+        Randomize()
+        Dim value5 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value5) = 1
+
+        Randomize()
+        Dim value6 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value6) = 1
+
+        PictureBox1.Visible = vis(0)
+        PictureBox2.Visible = vis(1)
+        PictureBox3.Visible = vis(2)
+        PictureBox4.Visible = vis(3)
+        PictureBox5.Visible = vis(4)
+        PictureBox6.Visible = vis(5)
+        PictureBox7.Visible = vis(6)
+        PictureBox8.Visible = vis(7)
+        PictureBox9.Visible = vis(8)
+
+
+        If (clicked(6) = False) Then
+            contador = contador + 1
+            clicked(6) = True
+        End If
+
+        If (contador = 8) Then
+            MsgBox("Has ganado")
+        End If
+
+    End Sub
+
+    Private Sub PictureBox8Click(sender As Object, e As EventArgs) Handles PictureBox8.Click
+
+        vis(0) = PictureBox1.Visible
+        vis(1) = PictureBox2.Visible
+        vis(2) = PictureBox3.Visible
+        vis(3) = PictureBox4.Visible
+        vis(4) = PictureBox5.Visible
+        vis(5) = PictureBox6.Visible
+        vis(6) = PictureBox7.Visible
+        vis(7) = PictureBox8.Visible
+        vis(8) = PictureBox9.Visible
+
+        Randomize()
+        Dim value1 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value1) = 0
+        Randomize()
+        Dim value2 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value2) = 0
+        Randomize()
+        Dim value3 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value3) = 0
+        Randomize()
+        Dim value4 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value4) = 1
+
+        Randomize()
+        Dim value5 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value5) = 1
+
+        Randomize()
+        Dim value6 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value6) = 1
+
+        PictureBox1.Visible = vis(0)
+        PictureBox2.Visible = vis(1)
+        PictureBox3.Visible = vis(2)
+        PictureBox4.Visible = vis(3)
+        PictureBox5.Visible = vis(4)
+        PictureBox6.Visible = vis(5)
+        PictureBox7.Visible = vis(6)
+        PictureBox8.Visible = vis(7)
+        PictureBox9.Visible = vis(8)
+
+
+        If (clicked(7) = False) Then
+            contador = contador + 1
+            clicked(7) = True
+        End If
+
+        If (contador = 8) Then
+            MsgBox("Has ganado")
+        End If
+
+    End Sub
+
+    Private Sub PictureBox9_Click(sender As Object, e As EventArgs) Handles PictureBox9.Click
+
+        vis(0) = PictureBox1.Visible
+        vis(1) = PictureBox2.Visible
+        vis(2) = PictureBox3.Visible
+        vis(3) = PictureBox4.Visible
+        vis(4) = PictureBox5.Visible
+        vis(5) = PictureBox6.Visible
+        vis(6) = PictureBox7.Visible
+        vis(7) = PictureBox8.Visible
+        vis(8) = PictureBox9.Visible
+
+        Randomize()
+        Dim value1 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value1) = 0
+        Randomize()
+        Dim value2 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value2) = 0
+        Randomize()
+        Dim value3 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value3) = 0
+        Randomize()
+        Dim value4 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value4) = 1
+
+        Randomize()
+        Dim value5 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value5) = 1
+
+        Randomize()
+        Dim value6 As Integer = CInt(Int((8 * Rnd()) + 0))
+
+        vis(value6) = 1
+
+        PictureBox1.Visible = vis(0)
+        PictureBox2.Visible = vis(1)
+        PictureBox3.Visible = vis(2)
+        PictureBox4.Visible = vis(3)
+        PictureBox5.Visible = vis(4)
+        PictureBox6.Visible = vis(5)
+        PictureBox7.Visible = vis(6)
+        PictureBox8.Visible = vis(7)
+        PictureBox9.Visible = vis(8)
+
+
+        If (clicked(8) = False) Then
+            contador = contador + 1
+            clicked(8) = True
+        End If
+
+        If (contador = 8) Then
+            MsgBox("Has ganado")
+        End If
+
+    End Sub
+
+End Class
